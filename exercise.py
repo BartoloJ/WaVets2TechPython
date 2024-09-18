6f109d2ae7a168659e021d1335d8bf5665e45266
BartoloJ <112433279+BartoloJ@users.noreply.github.com>
2024-09-18 10:07:27 -0700

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,28 @@
+def computepay(hours, rate):
+    if hours > 40:
+        overtime_hours = hours - 40
+        normal_pay = 40 * rate
+        overtime_pay = overtime_hours * (rate * 1.5)
+        pay = normal_pay + overtime_pay
+    else:
+        pay = hours * rate
+    return pay
+
+hours = input('Enter Hours:\n')
+try:
+    hours = float(hours)
+except:
+    print('ERROR', hours, 'is not a valid number')
+    quit()
+rate = input('Enter Rate:\n')
+try:
+    rate = float(rate)
+    
+except:
+    print('EROR', rate, 'is not a valid number')
+    quit()
+
+
+
+print('Your pay:', computepay(hours, rate))
+
